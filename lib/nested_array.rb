ddb3ec386440d165e613bcc75c7925f5f1bd3073
thomas dubiel <td1729@hotmail.com>
2019-10-31 19:17:19 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -29,6 +29,15 @@ def array_literal_matrix
   # Using Array literal syntax only, build a nested array that uses the data in
   # side the ENNET_HOUSE and ENFIELD_TENNIS_ACADEMY Arrays but sorts
   # alphabetically by the first character.
+  residents = {
+    [
+  "Don Gately",
+  "Joelle van Dyne",
+  "Pat M.",
+  "Kate Gompert",
+  "Bruce Green"
+    ]
+  }
 end
 
 def matrix_lookup(matrix, row, column)
